dae114e319b6c0c25a0239fe5db52c0b6a795d56
FivePood <ps220790@gmail.com>
2021-01-10 19:08:22 +0300

    Admin panel (Gii, Migration)

---
+++ backend/models/Article.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $content
+ * @property string|null $date
+ * @property int|null $user_id
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'date' => 'Date',
+            'user_id' => 'User ID',
+        ];
+    }
+}
---
+++ backend/modules/admin/views/default/index.php
@@ -0,0 +1,8 @@
+<div class="admin-default-index">
+    <form action="/admin/article">
+        <input type="button" class="btn btn-primary" value="Articles" />
+    </form>
+    <form action="/admin/article">
+        <input type="button" class="btn btn-primary" value="Users" />
+    </form>
+</div>

3afb12eada7880391481d39a2bc5cfe3c5a022f3
FivePood <ps220790@gmail.com>
2021-01-31 19:48:30 +0300

    PrimaryKey; ForeignKeys; camelCase

--- backend/modules/admin/views/default/index.php
+++
@@ -1,8 +0,0 @@
-<div class="admin-default-index">
-    <form action="/admin/article">
-        <input type="button" class="btn btn-primary" value="Articles" />
-    </form>
-    <form action="/admin/article">
-        <input type="button" class="btn btn-primary" value="Users" />
-    </form>
-</div>
